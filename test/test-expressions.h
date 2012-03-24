58112301616bac535de6d3337ca847d1b500fa95
Matt Bierner <mattbierner@gmail.com>
2012-03-24 11:42:18 -0400

    Added very simple str expr tests.

--- test/test-expressions.h
+++ test/test-expressions.h
@@ -6,6 +6,7 @@
 
 extern CU_SuiteInfo pro_expr_test_suite;
 extern CU_SuiteInfo pro_identifier_expr_test_suite;
+extern CU_SuiteInfo pro_string_expr_test_suite;
 extern CU_SuiteInfo pro_let_expr_test_suite;
 
 #endif
---
+++ test/test-pro_string_expr.c
@@ -0,0 +1,48 @@
+#include "test-expressions.h"
+
+#include "prosopon.h"
+
+#include "pro_expr.h"
+#include "pro_string_expr.h"
+
+static pro_state* state;
+
+
+static int init(void)
+{
+    state = pro_state_create();
+    return state == 0;
+}
+
+static int cleanup(void)
+{
+    pro_state_release(state);
+    state = 0;
+    return 0;
+}
+
+
+#pragma mark -
+#pragma mark Tests
+
+static void test_create(void)
+{
+    char* str = "abcd \n";
+    pro_expr* t = pro_string_expr_create(str);
+    CU_ASSERT(pro_expr_get_type(t) == PRO_STRING_EXPR_TYPE);
+    CU_ASSERT_STRING_EQUAL(str, t->value.identifier);
+}
+
+
+static CU_TestInfo tests[] = {
+    {"create", test_create},
+    CU_TEST_INFO_NULL,
+};
+
+
+CU_SuiteInfo pro_string_expr_test_suite = {
+    "pro_string_expr",
+    init,
+    cleanup,
+    tests
+};
