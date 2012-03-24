6f462b13ccb1e4bdb82fbb312f791b578ba3d5e1
Matt Bierner <mattbierner@gmail.com>
2012-03-24 13:33:29 -0400

    Added basic let constructor behavior.

--- src/pro_let_expr.c
+++ src/pro_let_expr.c
@@ -4,7 +4,57 @@
 
 #include <assert.h>
 #include <stdio.h>
+#include <stdlib.h>
 
+
+typedef struct {
+    pro_env_lookup* env;
+    pro_expr* constructor_expr;
+    pro_expr* actor_expr;
+} constructor_data;
+
+static void bind_arguments(pro_state* s, pro_expr_list* id_list, pro_lookup_list* values)
+{
+    pro_expr_list* list = id_list;
+    pro_lookup_list* lookup_list;
+    while (list)
+    {
+        pro_expr* value = list->value;
+        pro_lookup* lookup = lookup_list->value;
+        if (value)
+        {
+            assert(pro_expr_get_type(value) == PRO_IDENTIFIER_EXPR_TYPE);
+            pro_bind(s, lookup, value->value.identifier);
+        }
+        id_list = id_list->next;
+        lookup_list = lookup_list->next;
+    }
+}
+
+static pro_lookup* contructor(pro_state* s, pro_lookup_list* arguments, void* d)
+{
+    pro_lookup* actor = 0;
+    const constructor_data* data = d;
+    
+    pro_expr* constructor_expr = data->constructor_expr;
+    pro_expr* actor_expr = data->actor_expr;
+
+    // Create a new environment and make it current.
+    pro_env_lookup* env = pro_env_create(s, data->env);
+    pro_push_env(s, env);
+    
+    // bind all arguments in the new environment
+    bind_arguments(s, constructor_expr->value.constructor.arguments, arguments);
+    
+    // Create a new actor in the new environment.
+    actor = pro_actor_create(s);
+    pro_actor_become(s, actor, pro_actor_expr_get_behavior(s, actor_expr));
+
+    // Restore the old environment.
+    pro_pop_env(s);
+    
+    return actor;
+}
 
 static void let_expr_eval(pro_state* s, pro_expr* t)
 {
@@ -20,8 +70,13 @@ static void let_expr_eval(pro_state* s, pro_expr* t)
         pro_bind(s, right->data.lookup, left->value.identifier);
         break;
     case PRO_CONSTRUCTOR_EXPR_TYPE:
-        
-        break;
+    {
+        constructor_data* data = malloc(sizeof(*data));
+        data->env = pro_get_env(s);
+        data->constructor_expr = left;
+        data->actor_expr = right;
+        pro_constructor_create(s, contructor, data);
+    }   break;
     default:
         assert(0);
         break;
