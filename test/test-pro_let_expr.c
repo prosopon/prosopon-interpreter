9067a3ade60151c6260de6709970c2748712c97f
Matt Bierner <mattbierner@gmail.com>
2012-03-24 12:43:18 -0400

    Simple let expr test.

--- test/test-pro_let_expr.c
+++ test/test-pro_let_expr.c
@@ -2,6 +2,10 @@
 
 #include "prosopon.h"
 
+#include "pro_let_expr.h"
+#include "pro_identifier_expr.h"
+#include "pro_actor_expr.h"
+
 
 static pro_state* state;
 
@@ -23,13 +27,25 @@ static int cleanup(void)
 #pragma mark -
 #pragma mark Tests
 
-static void test_create(void)
+static void test_create_identifier_actor(void)
 {
-    //pro_expr()
+    pro_expr* identifier = pro_identifier_expr_create("id");
+    pro_expr* actor = pro_actor_expr_create(0);
+    pro_expr* t = pro_let_expr_create(identifier, actor);
+    CU_ASSERT(t != 0);
+    CU_ASSERT(pro_expr_get_type(t) == PRO_LET_EXPR_TYPE);
+    
+    pro_expr* left = t->value.binary.left;
+    CU_ASSERT(left != 0);
+    CU_ASSERT(pro_expr_get_type(left) == PRO_IDENTIFIER_EXPR_TYPE);
+    
+    pro_expr* right = t->value.binary.right;
+    CU_ASSERT(right != 0);
+    CU_ASSERT(pro_expr_get_type(right) == PRO_ACTOR_EXPR_TYPE);
 }
 
 static CU_TestInfo tests[] = {
-    {"create", test_create},
+    {"create_identifier_actor", test_create_identifier_actor},
     CU_TEST_INFO_NULL,
 };
 
